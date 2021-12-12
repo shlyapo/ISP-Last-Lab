f46941ada709b974886b31c6275c3b8f87c7f691
shlyapo <72437851+shlyapo@users.noreply.github.com>
2021-12-12 12:44:05 +0300

    Add files via upload

---
+++ Auto/Autopark.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Auto
+{
+    public class Autopark
+    {
+        public byte ID { get; set; }
+
+        public string Name { get; set; }
+        public int CarInspection { get; set; }
+        public static byte TotalInspection { get; set; }
+
+        public Autopark(byte ID, string Name, int CarInspection)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.CarInspection = CarInspection;
+
+        }
+        public static bool Checks(Autopark el)
+        {
+            if (el.CarInspection == 0)
+                return false;
+            ++TotalInspection;
+            return true;
+        }
+
+
+    }
+}
---
+++ Auto/StreamService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+namespace Auto
+{
+    public class StreamService
+    {
+        private object locker = new object();
+        public Task WriteToStream(Stream stream)
+        {
+            return Task.Run(() =>
+            {
+                var Rand = new Random();
+                lock (locker)
+                {
+                    Console.WriteLine($"Writing to stream [{Thread.CurrentThread.ManagedThreadId}]");
+                    var streamWriter = new StreamWriter(stream) { AutoFlush = true };
+                    for (byte i = 0; i < 100; i++)
+                        streamWriter.WriteLine(i + "\nCar Model #{" + i + "}\n" + Rand.Next(0, 1));
+                    Console.WriteLine($"Writing to stream end [{Thread.CurrentThread.ManagedThreadId}]");
+
+                }
+
+            }
+            );
+        }
+
+        public Task CopyFromStream(Stream stream, string fileName)
+        {
+
+            return Task.Run(() =>
+            {
+                lock (locker)
+                {
+                    Console.WriteLine($"Copying from stream... [{Thread.CurrentThread.ManagedThreadId}]");
+                    stream.Seek(0, SeekOrigin.Begin);
+                    if (File.Exists(fileName)) File.Delete(fileName);
+                    using var fileStream = File.Open(fileName, FileMode.Create);
+                    stream.CopyTo(fileStream);
+                    Console.WriteLine($"Copying from stream end [{Thread.CurrentThread.ManagedThreadId}]");
+                }
+
+            });
+        }
+
+        public async Task<int> GetStatisticsAsync(string filename, Func<Autopark, bool> filter) =>
+            await Task.Run(() => GetStatistics(filename, filter));
+
+        public async Task<int> GetStatistics(string fileName, Func<Autopark, bool> filter)
+        {
+            int count = 0;
+            Console.WriteLine($"Calculating stats... [{Thread.CurrentThread.ManagedThreadId}]");
+            var streamReader = new StreamReader(File.Open(fileName, FileMode.Open));
+            List<Autopark> autos = new List<Autopark>();
+            for (var i = 0; i < 100; i++)
+            {
+
+                autos.Add(new Autopark(Convert.ToByte(await streamReader.ReadLineAsync()), await streamReader.ReadLineAsync(), Convert.ToInt32(await streamReader.ReadLineAsync())));
+                if (filter(autos[i])) count++;
+                Console.WriteLine(autos[i].Name);
+
+            }
+            streamReader.Dispose();
+            Console.WriteLine($"Calculating stats end [{Thread.CurrentThread.ManagedThreadId}]");
+            return count;
+
+        }
+        public StreamService()
+        {
+        }
+    }
+}
